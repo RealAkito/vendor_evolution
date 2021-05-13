9b49056339b7b0d20e2f293c1f750622859c5056
Akito Mizukito <akito@evolution-x.org>
2021-05-13 15:57:26 +0000

    Revert "Revert "[1/2] evolution: Add a guard for all kernel-related tasks to check for prebuilt kernel""
    This reverts commit a623b453848291e604ad676e2b93bee3fdac1ab0.
    
    Change-Id: I3de103cb4838f22b763f8067f4f663abfbc1f6f9
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -73,6 +73,7 @@
 #                                          kernel sources are present
 
 ifneq ($(TARGET_NO_KERNEL),true)
+ifeq ($(LOCAL_KERNEL),)
 
 ## Externally influenced variables
 KERNEL_SRC := $(TARGET_KERNEL_SOURCE)
@@ -503,4 +504,5 @@ dtboimage: $(INSTALLED_DTBOIMAGE_TARGET)
 .PHONY: dtbimage
 dtbimage: $(INSTALLED_DTBIMAGE_TARGET)
 
+endif # LOCAL_KERNEL
 endif # TARGET_NO_KERNEL
